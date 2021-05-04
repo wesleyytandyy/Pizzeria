66fd53d4a9fd49e019ada541fad574ad9738a15e
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-05-03 19:36:20 -0500

    got tabs to work

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -5,4 +5,6 @@ app_name = 'pizzas'
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('zas', views.zas, name='zas'),
+    path('za/<int:pizza_id>/', views.za, name="za")
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Pizza, Topping
 
 # Create your views here.
 
@@ -6,3 +7,20 @@ from django.shortcuts import render
 def index(request):
     """ The home page for Pizzeria """
     return render(request, 'pizzas/index.html')
+
+def zas(request):
+    pizza = Pizza.objects.order_by('name')
+    #entries = topic.entry_set.order_by('-date_added')
+
+    context = {'pizza': pizza}
+
+
+    return render(request, 'pizzas/zas.html', context)
+
+def za(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    topping = Topping.entry_set.order_by('name')
+    context = {'pizza': pizza, 'topping': topping}
+
+    return render(request, 'pizzas/za.html', context)
